c053b798e3253d35ad7840694eb6ff2eb0b0ea17
mitchel <edahmitchel@example.com>
2022-11-24 22:03:18 +0100

    edited new build

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,24 @@ app.get("/api/chat/:id", (req, res) => {
 });
 app.use("/api/users", userRoutes);
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
@@ -74,24 +92,6 @@ io.on("connection", (socket) => {
     socket.leave(userData._id);
   });
 });
-
-// --------------------------deployment------------------------------
-
-const __dirname1 = path.resolve();
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
-  });
-}
-
-// --------------------------deployment------------------------------
 // ------ deployment--------
 const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {

22315a5c2355853fe1805d445b700e5f4d639189
edahmitchel <edahmitchel@gmail.com>
2023-02-28 13:33:04 +0100

    bjhfgyfgu

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,5 @@
 const asyncHandler = require("express-async-handler");
-const generateToken = require("../config/generateToken.js");
+const generateToken = require("../config/generateToken");
 const { User } = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const {
